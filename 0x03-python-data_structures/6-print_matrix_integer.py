853144cfe990e39231105e22a02a5a82246036ad
MugaboImma <emugabo21@yahoo.com>
2023-05-14 16:09:39 -0700

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
     for ro in matrix:
-        for col in matrix:
+        for col in ro:
             print("{:d}".format(col), end="" if col != ro[-1] else "")
         print()
