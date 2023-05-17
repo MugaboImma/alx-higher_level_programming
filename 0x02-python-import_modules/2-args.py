4e418546750b206ccaf10291aaf389ba91b761d9
MugaboImma <emugabo21@yahoo.com>
2023-05-17 16:16:52 -0700

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+# A program that prints the number of and the list of its arguments.
+
+if __name__ == "__main__":
+    import sys
+
+    num_args = len(sys.argv) - 1
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(num_args))
+
+    for i in range(num_args):
+        print("{}:{}".format(i + 1, sys.argv[i + 1]))
