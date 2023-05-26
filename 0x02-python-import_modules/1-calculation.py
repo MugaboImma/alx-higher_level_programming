addf7efeba2d2be768ccb55be7a0b9cd83090c0a
MugaboImma <emugabo21@yahoo.com>
2023-05-26 16:27:02 -0700

    1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
+#import func from file given, does Maths and prints result
 
 if __name__ == "__main__":
-    from calculator_1 import sub, add, mul, div
+    from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
-    print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
