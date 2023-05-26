5edbbd5c500cd3772d7c66a725ff3bde5ec314cb
MugaboImma <emugabo21@yahoo.com>
2023-05-26 16:39:54 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-# A program that imports the function def add(a, b): from
-# the file add_0.py and prints the result of the addition 1 + 2 = 3
-
 if __name__ == "__main__":
     from add_0 import add
 
