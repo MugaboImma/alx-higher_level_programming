888ab1f4d4445df353ae5681d50e5c5eab0096f6
MugaboImma <emugabo21@yahoo.com>
2023-05-29 14:16:29 -0700

    100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+A program that imports all functions from the file calculator_1.py and handles basic operations.
+"""
+if __name__ =="__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    argc = len(argv)
+    if argc != 4:
+        print('Usage: {} <a> <operator> <b>'.format(argv[0]))
+        exit(1)
+    oper = {
+            '+': add,
+            '-': sub,
+            '*': mul,
+            '/': div
+            }
+    if argv[2] in oper:
+        num_1 = int(argv[1])
+        num_2 = int(argv[3])
+        op = oper[argv[2]]
+        res = op(num_1, num_2)
+        print('{:d} {:s} {:d} = {:d}'.format(num_1, argv[2], num_2, res))
+    else:
+        print('Unkown operator. Available operators: +, -, * and /')
+        exit(1)
+    exit(0)
