28353e36244c5d598ae974b9b6aed2d03340a3ff
MugaboImma <emugabo21@yahoo.com>
2023-05-29 09:01:14 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A function that prints x elements of a list.
+The function will Returns the real number of elements printed
+"""
+def safe_print_list(my_list=[], x=0):
+    new_list = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            new_list += 1
+        except IndexError:
+            break
+    print("")
+    return (new_list)
+
