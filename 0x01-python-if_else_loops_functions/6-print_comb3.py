42632497965940238d854ef83e5d5b0b4ad024c7
MugaboImma <emugabo21@yahoo.com>
2023-05-31 06:19:11 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for dig_1 in range(0, 10):
+for dig_1 in range(0, 9):
     for dig_2 in range(dig_1 + 1, 10):
-        if dig_1 == 8 and dig_2 == 9:
-            print("{}{}".format(dig_1, dig_2))
+        if dig_1 is not 8 or dig_2 is not 9:
+            print("{:d}{:d}".format(dig_1, dig_2), end=", ")
         else:
-            print("{}{}".format(dig_1, dig_2), end=",")
+            print("{:d}{:d}".format(dig_1, dig_2))
