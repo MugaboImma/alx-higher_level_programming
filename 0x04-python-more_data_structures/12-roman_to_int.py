40d29dc67da06bf220dc764e6748d7916ce5cf85
MugaboImma <emugabo21@yahoo.com>
2023-05-31 08:45:30 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"""a function def roman_to_int(roman_string): that converts a Roman numeral to an integer."""
+
+""" a function def roman_to_int(roman_string):
+that converts a Roman numeral to an integer."""
 
 def roman_to_int(roman_string: str):
-    if (roman_string) == None or type(roman_string) != str:
+    
+    if (roman_string) is None or type(roman_string) != str:
         return 0
-    dict_num = {'I': 1, 'V': 5, 'X': 10, 'X': 10, 'L': 50, 'C': 100, 'D':500, 'M':1000}
+    dict_num = {'I': 1, 'V': 5, 'X': 10, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000}
     num = [dict_num[i] for i in roman_string] + [0]
     rep = 0
 
